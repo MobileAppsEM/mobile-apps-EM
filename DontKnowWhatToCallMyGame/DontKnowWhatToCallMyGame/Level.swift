8448070f7136fc4c33abbd6c64d8519c82df697a
Scal4 <34249624+Scal4@users.noreply.github.com>
2018-04-11 11:31:06 -0500

    Add files via upload

--- DontKnowWhatToCallMyGame/DontKnowWhatToCallMyGame/Level.swift
+++ DontKnowWhatToCallMyGame/DontKnowWhatToCallMyGame/Level.swift
@@ -35,19 +35,54 @@ class Level
         wallTop.size.height = 20
         wallBottom.size.height = 20
         
-        wallRight.position.x = SKscene.position.x - SKscene.size.width / 2 + wallRight.size.width / 2
+        wallRight.position.x = SKscene.position.x + SKscene.size.width / 2 - wallRight.size.width / 2
         
-        wallBottom.position.y = SKscene.position.y + SKscene.size.height - wallLeft.size.height
+        wallLeft.position.x = SKscene.position.x - SKscene.size.width / 2 + wallRight.size.width / 2
+        
+        wallBottom.position.y = SKscene.position.y - SKscene.size.height / 2 + wallBottom.size.height / 2
+        wallTop.position.y = SKscene.position.y + SKscene.size.height / 2 - wallTop.size.height / 2
+        
         
         wallLeft.color = UIColor.cyan
         wallTop.color = UIColor.cyan
         wallBottom.color = UIColor.cyan
         wallRight.color = UIColor.cyan
         
-        //SKscene.addChild(wallLeft)
+        wallLeft.physicsBody = SKPhysicsBody(rectangleOf: wallLeft.frame.size)
+        wallLeft.physicsBody?.restitution = 0
+        wallLeft.physicsBody?.friction = 0.1
+        
+        wallTop.physicsBody = SKPhysicsBody(rectangleOf: wallTop.frame.size)
+        wallTop.physicsBody?.restitution = 0
+        wallTop.physicsBody?.friction = 0.1
+        
+        wallBottom.physicsBody = SKPhysicsBody(rectangleOf: wallBottom.frame.size)
+        wallBottom.physicsBody?.restitution = 0
+        wallBottom.physicsBody?.friction = 0.1
+        
+        wallRight.physicsBody = SKPhysicsBody(rectangleOf: wallRight.frame.size)
+        wallRight.physicsBody?.restitution = 0
+        wallRight.physicsBody?.friction = 0.1
+        
+        wallLeft.physicsBody?.affectedByGravity = false
+        wallTop.physicsBody?.affectedByGravity = false
+        wallBottom.physicsBody?.affectedByGravity = false
+        wallRight.physicsBody?.affectedByGravity = false
+        
+        wallLeft.physicsBody?.isDynamic = false
+        wallTop.physicsBody?.isDynamic = false
+        wallBottom.physicsBody?.isDynamic = false
+        wallRight.physicsBody?.isDynamic = false
+        
+        wallLeft.physicsBody?.allowsRotation = false
+        wallTop.physicsBody?.allowsRotation = false
+        wallBottom.physicsBody?.allowsRotation = false
+        wallRight.physicsBody?.allowsRotation = false
+        
+        SKscene.addChild(wallLeft)
         SKscene.addChild(wallRight)
-        //SKscene.addChild(wallTop)
-        // SKscene.addChild(wallBottom)
+        SKscene.addChild(wallTop)
+        SKscene.addChild(wallBottom)
         
     
     }
